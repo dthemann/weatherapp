9828a3c6cdf00874574097a41dd79c134c69158a
Dominik Mann <ic21b035@technikum-wien.at>
2023-01-21 14:38:31 +0100

    update frm Configuration

---
+++ src/main/java/dev/fhtw/oode/weatherapp/frm_configurateController.java
@@ -0,0 +1,126 @@
+package dev.fhtw.oode.weatherapp;
+
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class frm_configurateController {
+
+    @FXML
+    private Button bt_LoadConfig;
+
+    @FXML
+    private Button bt_SaveConfig;
+
+    @FXML
+    private Button bt_searchLocation;
+
+    @FXML
+    private Button bt_selectLocation;
+
+    @FXML
+    private FontAwesomeIconView bt_update_uprate;
+
+    @FXML
+    private Button bt_updatelocationAPIKey;
+
+    @FXML
+    private Button bt_updateweatherAPIKey;
+
+    @FXML
+    private Label lb_locsetup_log;
+
+    @FXML
+    private ListView<?> lv_Locations;
+
+    @FXML
+    private RadioButton rb_imperialunits;
+
+    @FXML
+    private RadioButton rb_metricunits;
+
+    @FXML
+    private TextField tb_locationAPIKey;
+
+    @FXML
+    private TextField tb_uprate_hh;
+
+    @FXML
+    private TextField tb_uprate_mm;
+
+    @FXML
+    private TextField tb_uprate_ss;
+
+    @FXML
+    private TextField tb_weatherAPIKey;
+
+    @FXML
+    private TextField tf_serachLocation;
+
+    @FXML
+    void bt_LoadConfigButtonClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void bt_SaveConfigButtonClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void bt_searchLocationButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void bt_selectLocationButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void bt_update_uprate_ButtonClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void bt_updatelocationAPIKey_ButtonClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void bt_updateweatherAPIKey_ButtonClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void rb_imperialunits_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void rb_metricunits_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tb_uprate_hh_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tb_uprate_mm_Action(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tb_uprate_ss_Action(ActionEvent event) {
+
+    }
+
+}
